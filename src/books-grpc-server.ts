efdc8c6492db42c13af2b9ec000061e58965bae1
Steve Ognibene <steve.ognibene@namely.com>
2023-01-23 17:51:23 -0500

    add books gRPC server

---
+++ src/books-grpc-server.ts
@@ -0,0 +1,43 @@
+import grpc from '@grpc/grpc-js';
+import protoLoader from '@grpc/proto-loader'
+
+const booksProtoPath = new URL('../protos/books.proto', import.meta.url).pathname
+const booksPackageDefinition = protoLoader.loadSync(
+    booksProtoPath,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+const booksProto = (grpc.loadPackageDefinition(booksPackageDefinition) as any).apollo_example.books;
+
+const bookData = [{
+    title: 'The Awakening',
+    author: 'Kate Chopin'
+},
+{
+    title: 'City of Glass1',
+    author: 'Paul Auster',
+}];
+
+function listBooks(call, callback) {
+    const response = {
+        books: bookData.filter(b => b.title.includes(call.request.title))
+    };
+    callback(null, response);
+}
+
+const binding = '0.0.0.0:50051';
+
+function main() {
+    const server = new grpc.Server();
+    server.addService(booksProto.Books.service, { listBooks: listBooks });
+    server.bindAsync(binding, grpc.ServerCredentials.createInsecure(), () => {
+        server.start();
+        console.log(`gRPC server listening on ${binding}`);
+    });
+}
+
+main();
